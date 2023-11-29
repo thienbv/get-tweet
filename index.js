de393100b02ba8e6528de793cf70204243d1a9e3
ThienBui <37298863+tb1601@users.noreply.github.com>
2023-11-29 17:21:42 +0700

    init get tweet by guest token

---
+++ index.js
@@ -0,0 +1,33 @@
+const axios = require("axios");
+const getGuestToken = async () => {
+  const { data } = await axios.post("https://twitter.com/guest/activate");
+  var guestToken = data.match(/gt=([^;]+)/);
+  return guestToken[1];
+};
+
+const getTweet = async (tweetID) => {
+  const gt = await getGuestToken();
+  const url = `https://api.twitter.com/graphql/5GOHgZe-8U2j5sVHQzEm9A/TweetResultByRestId?variables=%7B%22tweetId%22%3A%22${tweetID}%22%2C%22withCommunity%22%3Afalse%2C%22includePromotedContent%22%3Afalse%2C%22withVoice%22%3Afalse%7D&features=%7B%22creator_subscriptions_tweet_preview_api_enabled%22%3Atrue%2C%22c9s_tweet_anatomy_moderator_badge_enabled%22%3Atrue%2C%22tweetypie_unmention_optimization_enabled%22%3Atrue%2C%22responsive_web_edit_tweet_api_enabled%22%3Atrue%2C%22graphql_is_translatable_rweb_tweet_is_translatable_enabled%22%3Atrue%2C%22view_counts_everywhere_api_enabled%22%3Atrue%2C%22longform_notetweets_consumption_enabled%22%3Atrue%2C%22responsive_web_twitter_article_tweet_consumption_enabled%22%3Afalse%2C%22tweet_awards_web_tipping_enabled%22%3Afalse%2C%22responsive_web_home_pinned_timelines_enabled%22%3Atrue%2C%22freedom_of_speech_not_reach_fetch_enabled%22%3Atrue%2C%22standardized_nudges_misinfo%22%3Atrue%2C%22tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled%22%3Atrue%2C%22longform_notetweets_rich_text_read_enabled%22%3Atrue%2C%22longform_notetweets_inline_media_enabled%22%3Atrue%2C%22responsive_web_graphql_exclude_directive_enabled%22%3Atrue%2C%22verified_phone_label_enabled%22%3Afalse%2C%22responsive_web_media_download_video_enabled%22%3Afalse%2C%22responsive_web_graphql_skip_user_profile_image_extensions_enabled%22%3Afalse%2C%22responsive_web_graphql_timeline_navigation_enabled%22%3Atrue%2C%22responsive_web_enhance_cards_enabled%22%3Afalse%7D`;
+  let config = {
+    headers: {
+      authority: "api.twitter.com",
+      accept: "*/*",
+      origin: "https://twitter.com",
+      referer: "https://twitter.com/",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-site",
+      "user-agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+      "x-guest-token": gt,
+      "x-twitter-active-user": "yes",
+      Authorization:
+        "Bearer AAAAAAAAAAAAAAAAAAAAANRILgAAAAAAnNwIzUejRCOuH5E6I8xnZz4puTs%3D1Zv7ttfk8LF81IUq16cHjhLTvJu4FA33AGWWjCpTnA",
+      Cookie:
+        'guest_id=v1%3A170106814491761660; guest_id_ads=v1%3A170106814491761660; guest_id_marketing=v1%3A170106814491761660; personalization_id="v1_phmyh0x+ybSNNOoX6jsuNQ=="',
+    },
+  };
+  const { data } = await axios.get(url, { headers: config.headers });
+  console.log(JSON.stringify(data));
+  return data;
+};
+getTweet("1719734071243207033");
